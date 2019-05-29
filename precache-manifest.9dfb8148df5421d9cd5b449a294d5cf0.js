628bd0444aa25983f77b8df0de844050588a0a1c
SGeorges <SGeorges@github.com>
2019-05-29 09:55:40 -0400

    Updates

---
+++ precache-manifest.9dfb8148df5421d9cd5b449a294d5cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23869a10d452945189dfa2759ce15332",
+    "url": "/Click-y-Game/index.html"
+  },
+  {
+    "revision": "7e201eed6e188059d76a",
+    "url": "/Click-y-Game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Click-y-Game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7e201eed6e188059d76a",
+    "url": "/Click-y-Game/static/js/main.b665d14b.chunk.js"
+  },
+  {
+    "revision": "c861208a093aacbfff7e",
+    "url": "/Click-y-Game/static/js/runtime~main.00e7fac7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Click-y-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
